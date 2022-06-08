89c91da82b17e76d796da89206ae0ea3e6890f04
GeonwooVincentKim <kdsnop@gmail.com>
2022-06-08 21:32:33 +0900

    [Update] - Updated Footerv.vue file

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,7 +1,5 @@
 <template>
-  <footer class="mt-5 py-5 bg-dark text-white">
-    &copy; 2021
-  </footer>
+  <footer class="mt-5 py-5 bg-dark text-white">&copy; 2021</footer>
 </template>
 <script>
 export default {
