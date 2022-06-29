c4e1aa1420f37dc8cad30bedc1caabad6b7034bb
GeonwooVincentKim <kdsnop@gmail.com>
2022-06-29 11:09:41 +0900

    [Update] - Block port

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,11 @@
 //     transpileDependencies: true
 // })
 
-const target = 'http://127.0.0.1:3000'
+const target = 'http://localhost:3000'
 
 module.exports = {
     devServer: {
-        port: 8080,
+        // port: 8080,
         proxy: {
             '^/api': {
                 target,
